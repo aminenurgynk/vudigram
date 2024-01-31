7d5b27853822a1a7a85ee6172ab53fac4be011f4
Aminenur Göynük <amine.gynk@gmail.com>
2024-01-31 15:58:41 +0300

    added sidebar topbar ...

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,7 @@ useMutation,
 useQueryClient,
 useInfiniteQuery,
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount } from '../appwrite/api'
+import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewUser } from '@/types'
 
 
@@ -20,5 +20,11 @@ export const useSignInAccount = () => {
             email: string; 
             password: string;
         }) => signInAccount(user),
+    })
+}
+
+export const useSignOutAccount = () => {
+    return useMutation({
+        mutationFn: signOutAccount
     })
 }
