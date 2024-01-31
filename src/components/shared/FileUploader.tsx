ca511d6786a2528e6a225f30771ea15065476bfb
Aminenur Göynük <amine.gynk@gmail.com>
2024-01-31 17:33:15 +0300

    feat(upgrade): react-dropzone here, and FileUploder updated

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
 
 const FileUploader = () => {
-  return (
-    <div>FileUploader</div>
-  )
-}
+  const [fileUrl, setFileUrl] = useState('');
 
-export default FileUploader
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div
+      {...getRootProps()}
+      className="flex flex-center flex-col bg-dar-3 rounded-xl cursor-pointer"
+    >
+      <input {...getInputProps()} className="cursor-pointer" />
+      {fileUrl ? (
+        <div>test1</div>
+      ) : (
+        <div>test2</div>
+      )}
+    </div>
+  );
+};
+
+export default FileUploader;
