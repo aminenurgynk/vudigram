297327694b33e00a76fe17d8b0ea0dfa9aac83fd
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-04 13:35:25 +0300

    fix: fix to fomatDate method

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,23 +5,48 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function formatDate(dateString: string): string {
-  const currentDate = new Date();
-  const inputDate = new Date(dateString);
+export function formatDateString(dateString: string) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
 
-  const timeDifference = currentDate.getTime() - inputDate.getTime();
-  const secondsDifference = timeDifference / 1000;
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString("en-US", options);
 
-  if (secondsDifference < 60) {
-    return `${Math.floor(secondsDifference)} seconds ago`;
-  } else if (secondsDifference < 3600) {
-    const minutes = Math.floor(secondsDifference / 60);
-    return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`;
-  } else if (secondsDifference < 86400) {
-    const hours = Math.floor(secondsDifference / 3600);
-    return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
-  } else {
-    const days = Math.floor(secondsDifference / 86400);
-    return `${days} ${days === 1 ? 'day' : 'days'} ago`;
-  }
+  const time = date.toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+  });
+
+  return `${formattedDate} at ${time}`;
 }
+
+// 
+export const multiFormatDateString = (timestamp: string = ""): string => {
+  const timestampNum = Math.round(new Date(timestamp).getTime() / 1000);
+  const date: Date = new Date(timestampNum * 1000);
+  const now: Date = new Date();
+
+  const diff: number = now.getTime() - date.getTime();
+  const diffInSeconds: number = diff / 1000;
+  const diffInMinutes: number = diffInSeconds / 60;
+  const diffInHours: number = diffInMinutes / 60;
+  const diffInDays: number = diffInHours / 24;
+
+  switch (true) {
+    case Math.floor(diffInDays) >= 30:
+      return formatDateString(timestamp);
+    case Math.floor(diffInDays) === 1:
+      return `${Math.floor(diffInDays)} day ago`;
+    case Math.floor(diffInDays) > 1 && diffInDays < 30:
+      return `${Math.floor(diffInDays)} days ago`;
+    case Math.floor(diffInHours) >= 1:
+      return `${Math.floor(diffInHours)} hours ago`;
+    case Math.floor(diffInMinutes) >= 1:
+      return `${Math.floor(diffInMinutes)} minutes ago`;
+    default:
+      return "Just now";
+  }
+};
