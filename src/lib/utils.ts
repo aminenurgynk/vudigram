3641aef6b53bff7c22f5ca6d24b92dc84f19e778
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-04 13:24:53 +0300

    feat: create method for format post create date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(dateString: string): string {
+  const currentDate = new Date();
+  const inputDate = new Date(dateString);
+
+  const timeDifference = currentDate.getTime() - inputDate.getTime();
+  const secondsDifference = timeDifference / 1000;
+
+  if (secondsDifference < 60) {
+    return `${Math.floor(secondsDifference)} seconds ago`;
+  } else if (secondsDifference < 3600) {
+    const minutes = Math.floor(secondsDifference / 60);
+    return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`;
+  } else if (secondsDifference < 86400) {
+    const hours = Math.floor(secondsDifference / 3600);
+    return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
+  } else {
+    const days = Math.floor(secondsDifference / 86400);
+    return `${days} ${days === 1 ? 'day' : 'days'} ago`;
+  }
+}
