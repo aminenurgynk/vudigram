ab9f847fb6f10c8f9c25e14f80701b83e2680f25
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-04 14:24:01 +0300

    feat: add savePost function

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -209,3 +209,24 @@ export async function likePost(postId: string, likesArray: string[]) {
         console.log(error);
     }
 }
+
+export async function savePost(postId: string, userId: string) {
+    try {
+        const updatedPost = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.savesCollectionId,
+            ID.unique(),
+            {
+                user: userId,
+                post: postId,
+            }
+        )
+
+        if(!updatedPost) throw Error;
+
+        return updatedPost;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
