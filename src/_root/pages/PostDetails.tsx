a83101a8a4bf88c4857bba679e5b6a5488d9af07
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-08 15:18:59 +0300

    feat: start to create post details page

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,8 +1,19 @@
+import { useGetPostById } from "@/lib/react-query/queriesAndMutations"
+import { Loader } from "lucide-react";
+import { useParams } from "react-router-dom";}
 
 
 const PostDetails = () => {
+  const { id } = useParams()
+  const { data: post, isPending } = useGetPostById(id || '');  
   return (
-    <div>PostDetails</div>
+    <div className="post_details-container">
+      {isPending ? <Loader /> : (
+        <div className="post_details-card">
+          <img />
+        </div>
+      )}
+    </div>
   )
 }
 
