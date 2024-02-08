0c0cc2101b6cff83017afcb1e5da69ec32c2202d
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-08 21:55:26 +0300

    feat: searching work very good

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,8 +1,25 @@
+import { Models } from "appwrite";
+import Loader from "./Loader";
+import { searchPosts } from "@/lib/appwrite/api";
+import GridPostList from "./GridPostList";
+
+type SearchResultsProps = {
+  isSearchFetching: boolean;
+  searchPosts: Models.Document[];
+}
 
 
-const SearchResults = () => {
+const SearchResults = ({ isSearchFetching && searchPosts }: SearchResultsProps) => {
+
+  if(isSearchFetching) return <Loader />
+  if(searchPosts.documents.lenght > 0) {
+  if(searchedPosts && searchedPosts.documents.lenght > 0) {
+    return (
+      <GridPostList posts={searchedPosts.documents} />
+    )
+  }
   return (
-    <div>SearchResults</div>
+    <p className="text-light-4 mt-10 text-center w-full">No results found</p>
   )
 }
 
