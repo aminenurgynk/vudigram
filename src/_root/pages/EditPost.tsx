6641ca047529ad96011ec50e0e3e8f351584d38d
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-08 12:08:54 +0300

    docs: use useGetPostById

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -1,8 +1,13 @@
 import PostForm from "@/components/forms/PostForm"
+import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
+import { Loader } from "lucide-react";
 import { useParams } from "react-router-dom"
 
 const EditPost = () => {
   const { id } = useParams();
+  const { data: post, isPending} = useGetPostById(id || '');
+
+  if(isPending) return <Loader />
   
   return (
     <div className="flex flex-1">
@@ -17,7 +22,7 @@ const EditPost = () => {
           <h2 className="h3-bold md:h2-bold text-left w-full">Edit Post</h2>
         </div>
 
-        <PostForm />
+        <PostForm action="update" post={post}/>
       </div>
     </div>
   )
