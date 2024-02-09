70ecf96b8f2f472ec0155648ac26fa1748f6af88
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-09 13:21:12 +0300

    fix: comment 181 line , temproray solution

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -178,7 +178,7 @@ export async function getRecentPosts() {
   const posts = await databases.listDocuments(
     appwriteConfig.databaseId,
     appwriteConfig.postCollectionId,
-    [Query.orderDesc("$createdAt", Query.limit(20))]
+    //[Query.orderDesc("$createdAt", Query.limit(20))]
   );
 
   if (!posts) throw Error;
