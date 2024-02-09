d702d2a1e2ae35d9f087b85e200deb99bbf727d3
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-09 13:22:33 +0300

    fix: one more temprorary solution

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -12,6 +12,8 @@ const Home = () => {
       <div className="home-container">
         <div className="home-posts">
           <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          
+      {isErrorPosts}
           {isPostLoading && ! posts ? (
             <Loader />
           ) : (
